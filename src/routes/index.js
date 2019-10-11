0ff865b98d18458507ca1d9cb003159f5b56460d
Franco Rossi <frossi@sofrecom.com.ar>
2019-10-11 12:09:39 -0300

    partials

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,8 +11,8 @@ router.get('/contact', (req, res) => {
     res.render('contact', { "title": "Contact page"});
 })
 
-router.get('/', (req, res) => {
-    res.render('index', { "title": "First website"});
+router.get('/about', (req, res) => {
+    res.render('about', { "title": "About page"});
 })
 
 module.exports = router;
